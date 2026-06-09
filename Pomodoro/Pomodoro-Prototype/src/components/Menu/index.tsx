277232ab8bc5b63cc681e0bfdcee10cc6129bf75
jtigik <jeftigik@hotmail.com>
2026-06-09 02:51:08 -0300

    Create a new component 'Menu'

---
+++ Pomodoro/Pomodoro-Prototype/src/components/Menu/index.tsx
@@ -0,0 +1,21 @@
+import { HistoryIcon, SettingsIcon, SunIcon, TimerIcon } from "lucide-react";
+import style from "./styles.module.css";
+
+export function Menu() {
+    return (
+        <nav className={style.menu}>
+            <a className={style.menuLink} href='#'>
+                <TimerIcon />
+            </a>
+            <a className={style.menuLink} href='#'>
+                <HistoryIcon />
+            </a>
+            <a className={style.menuLink} href='#'>
+                <SettingsIcon />
+            </a>
+            <a className={style.menuLink} href='#'>
+                <SunIcon />
+            </a>
+        </nav>
+    );
+}
