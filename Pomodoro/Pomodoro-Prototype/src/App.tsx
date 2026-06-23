7d4c161dc2b0f661dd9608b83e5ac8cbb5ae89a2
Jeferson Tigik <jeftigik@hotmail.com>
2026-06-23 06:23:10 -0300

    Create Cycles Component

--- Pomodoro/Pomodoro-Prototype/src/App.tsx
+++ Pomodoro/Pomodoro-Prototype/src/App.tsx
@@ -1,5 +1,6 @@
 import { Container } from "./components/Container";
 import { CountDown } from "./components/CountDown";
+import { Cycles } from "./components/Cycles";
 import { DefaultInput } from "./components/DefaultInput";
 import { Heading } from "./components/Heading";
 import { Logo } from "./components/Logo";
@@ -39,8 +40,7 @@ export function App() {
                         <p>Lorem ipsum dolor sit amet consectetur.</p>
                     </div>
                     <div className="formRow">
-                        <p>Ciclos</p>
-                        <p>0 0 0 0 0 0</p>
+                        <Cycles />
                     </div>
                     <div className="formRow">
                         <button type="submit">Add Task</button>
