cf31c8315c39fa6a28d7d16a0412a81a6d355ed1
Jeferson Tigik <jeftigik@hotmail.com>
2026-07-02 05:32:20 -0300

    Code cleaning

--- Pomodoro/Pomodoro-Prototype/src/App.tsx
+++ Pomodoro/Pomodoro-Prototype/src/App.tsx
@@ -4,12 +4,13 @@ import { CountDown } from "./components/CountDown";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { DefaultInput } from "./components/DefaultInput";
+import { Footer } from "./components/Footer";
 import { Logo } from "./components/Logo";
 import { Menu } from "./components/Menu";
 
-import { Footer } from "./components/Footer";
 import "./styles/global.css";
 import "./styles/theme.css";
+
 
 export function App() {
 
@@ -17,7 +18,7 @@ export function App() {
         <>
             <Container>
                 <Logo />
-            </Container>""
+            </Container>
 
             <Container>
                 <Menu />
