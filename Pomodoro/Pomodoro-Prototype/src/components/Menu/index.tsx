f79b7fafca5a3c9b8089fcb44fc3f7cb7bd38c4e
Jeferson Tigik <jeftigik@hotmail.com>
2026-07-06 06:34:14 -0300

    Capturando o evento de clique em link com preventDefault para tema dark e light

--- Pomodoro/Pomodoro-Prototype/src/components/Menu/index.tsx
+++ Pomodoro/Pomodoro-Prototype/src/components/Menu/index.tsx
@@ -1,19 +1,58 @@
 import { HistoryIcon, SettingsIcon, SunIcon, TimerIcon } from "lucide-react";
+import React, { useState } from "react";
 import style from "./styles.module.css";
 
+type AvailableThemes = 'dark' | 'light';
+
 export function Menu() {
+    const [theme, setTheme] = useState<AvailableThemes>("dark");
+
+    function handleThemeChange(
+        event: React.MouseEvent<HTMLAnchorElement, MouseEvent>,
+    ) {
+        event.preventDefault(); //Não seguir o link clicado
+
+        setTheme(prevTheme => {
+            const nextTheme = prevTheme === "dark" ? "light" : "dark";
+            return nextTheme;
+        });
+
+        document.documentElement.setAttribute("data-theme", theme);
+    }
     return (
         <nav className={style.menu}>
-            <a className={style.menuLink} href='#'>
+            <h1>{theme}</h1>
+            <a 
+                className={style.menuLink} 
+                href='#'
+                aria-label="Ir para a Home"
+                title="Ir para a Home"
+            >
                 <TimerIcon />
             </a>
-            <a className={style.menuLink} href='#'>
+            <a
+                className={style.menuLink} 
+                href='#'
+                aria-label="Ver histórico de tarefas"
+                title="Ver histórico de tarefas"
+            >
                 <HistoryIcon />
             </a>
-            <a className={style.menuLink} href='#'>
+            <a 
+                className={style.menuLink} 
+                href='#'
+                aria-label="Ver configurações"
+                title="Ver configurações"
+            >
                 <SettingsIcon />
             </a>
-            <a className={style.menuLink} href='#'>
+            <a 
+                className={style.menuLink} 
+                href='#' 
+                onClick={handleThemeChange}
+                aria-label="Mudar tema"
+                title="Mudar tema"
+            >
                 <SunIcon />
             </a>
         </nav>
