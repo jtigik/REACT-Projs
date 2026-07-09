1fe460cbc0f90e41d616db4d5205c9986401b128
Jeferson Tigik <jeftigik@hotmail.com>
2026-07-08 23:43:07 -0300

    Reestruturando o Projeto com Templates e Pages

---
+++ Pomodoro/Pomodoro-Prototype/src/templates/MainTemplate/index.tsx
@@ -0,0 +1,37 @@
+import { Footer } from "../../components/Footer";
+import { Logo } from "../../components/Logo";
+import { Menu } from "../../components/Menu";
+
+type MainTemplateProps = {
+    children: React.ReactNode;
+};
+
+export function MainTemplate({ children }: MainTemplateProps) {
+    return (
+        <>
+            <Container>
+                <Logo />
+            </Container>
+
+            <Container>
+                <Menu />
+            </Container>
+
+            {children}
+
+            <Container>
+                <Footer />
+            </Container>
+
+            <MainTemplate>
+                <Container>
+                    <CountDown />
+                </Container>
+    
+                <Container>
+                    <MainForm />
+                </Container>
+            </MainTemplate>
+        </>
+    );
+}
