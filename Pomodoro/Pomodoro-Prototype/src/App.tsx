f041e333e8468224ee90168757f5f1bc37489dbf
Jeferson Tigik <jeftigik@hotmail.com>
2026-07-12 17:15:04 -0300

    Criando o Componente `GenericHTML` e as Páginas `AboutPomodoro` e `NotFound`

--- Pomodoro/Pomodoro-Prototype/src/App.tsx
+++ Pomodoro/Pomodoro-Prototype/src/App.tsx
@@ -1,15 +1,19 @@
-
+// import { Route, Routes } from "react-router-dom"
 import { Home } from "./pages/Home";
+// import { AboutPomodoro } from "./pages/AboutPomodoro";
 // import { NotFound } from "./pages/NotFound";
+
 import "./styles/global.css";
 import "./styles/theme.css";
-
 
 export function App() {
 
     return (
         <Home />
+        /* <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<AboutPomodoro />} />
+            <Route path="*" element={<NotFound />} />
+        </Routes> */
     );
-
-    // return <NotFound />;
 }
