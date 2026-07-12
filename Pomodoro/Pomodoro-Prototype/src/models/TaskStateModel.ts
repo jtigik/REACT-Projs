eafae49cd58a4621ddf8f9e89ea8cb04c045c42c
Jeferson Tigik <jeftigik@hotmail.com>
2026-07-12 18:46:31 -0300

    Definindo os models da aplicação `TaskModel` e `TaskStateModel`

---
+++ Pomodoro/Pomodoro-Prototype/src/models/TaskStateModel.ts
@@ -0,0 +1,15 @@
+import type { TaskModel } from "./TaskModel";
+
+// This file defines the TaskStateModel type, which represents the state of a task in the application.
+export type TaskStateModel = {
+    task: TaskModel[];
+    secondsRemaining: number; // Seconds remaining for the current task
+    formatedSecondsRemaining: string; // Formatted string of seconds remaining (e.g., "00:25:00")
+    activeTask: TaskModel | null; // The currently active task, or null if no task is active
+    currentCycle: number; // The current cycle number (e.g., 1 for the first cycle)
+    config: {
+        workTime: number; // Configured work time in minutes
+        shortBreakTime: number; // Configured short break time in minutes
+        longBreakTime: number; // Configured long break time in minutes
+    };
+};
