52798748ec30c6ea323ac6d925852ee1afc77d46
Jeferson Tigik <jeftigik@hotmail.com>
2026-08-13 00:00:02 -0300

    Pegando o type do próximo ciclo - Algoritmo similar ao FizzBuzz

--- Pomodoro/Pomodoro-Prototype/src/components/MainForm/index.tsx
+++ Pomodoro/Pomodoro-Prototype/src/components/MainForm/index.tsx
@@ -6,11 +6,14 @@ import { getNextCycle } from "../../utils/getNextCycle";
 import { Cycles } from "../Cycles";
 import { DefaultButton } from "../DefaultButton";
 import { DefaultInput } from "../DefaultInput";
+import { getNextCycleType } from "../../utils/getNextCycleType";
 
 export function MainForm() {
     const { state, setState } = useTaskContext();
     const taskNameInput = useRef<HTMLInputElement>(null);
+
     const nextCycle = getNextCycle(state.currentCycle);
+    const nextCycleType = getNextCycleType(nextCycle);
 
     function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -31,7 +34,7 @@ export function MainForm() {
             completeDate: null,
             interruptDate: null,
             duration: 1,
-            type: 'workTime',
+            type: nextCycleType,
         };
 
         const secondsRemaining = newTask.duration * 60;
