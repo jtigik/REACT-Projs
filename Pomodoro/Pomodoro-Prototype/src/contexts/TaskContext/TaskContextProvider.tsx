2c9e3743a061313c13db58a38bc9be05f4dd9963
Jeferson Tigik <jeftigik@hotmail.com>
2026-08-17 01:08:46 -0300

    Entendendo useReducer, reducer, dispatch e actions

--- Pomodoro/Pomodoro-Prototype/src/contexts/TaskContext/TaskContextProvider.tsx
+++ Pomodoro/Pomodoro-Prototype/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useReducer, useState } from "react";
 import { inicialTaskState } from "./inicialTaskState";
 import { TaskContext } from "./TaskContext";
 
@@ -9,13 +9,30 @@ type TaskProviderProps = {
 export function TaskContextProvider({ children }: TaskProviderProps) {
     const [state, setState] = useState(inicialTaskState);
 
-    useEffect(() => {
+    const [numero, dispatch] = useReducer((state, action) => {
+        console.log(state, action);
+
+        switch (action) {
+            case 'INCREMENT':
+                return state + 1;
+            case 'DECREMENT':
+                return state - 1;
+            case 'INITIAL_STATE':
+                return 0;
+        }
+
+        return state; // estado atual (não altera)
+    }, 0);
+    /* useEffect(() => {
         console.log(state);
-    }, [state]);
+    }, [state]); */
 
     return (
         <TaskContext.Provider value={{ state, setState }}>
-            {children}
+            <h1>O número é: {numero}</h1>
+            <button onClick={() => dispatch('INCREMENT')}>Incrementar</button>
+            <button onClick={() => dispatch('DECREMENT')}>Decrementar</button>
+            <button onClick={() => dispatch('INITIAL_STATE')}>ZERAR</button>
         </TaskContext.Provider>
     );
 }
