d0017449722f738ade14a990efc4ac9214c75918
Jeferson Tigik <jeftigik@hotmail.com>
2026-08-21 01:46:58 -0300

    Migrando o state de useState para useReducer

--- Pomodoro/Pomodoro-Prototype/src/components/MainForm/index.tsx
+++ Pomodoro/Pomodoro-Prototype/src/components/MainForm/index.tsx
@@ -1,20 +1,21 @@
-import { PlayCircleIcon, StopCircleIcon } from "lucide-react";
-import { useRef } from "react";
-import { useTaskContext } from "../../contexts/TaskContext/useTaskContext";
-import type { TaskModel } from "../../models/TaskModel";
-import { formatSecondsToMinutes } from "../../utils/formatSecondsToMinutes";
-import { getNextCycle } from "../../utils/getNextCycle";
-import { getNextCycleType } from "../../utils/getNextCycleType";
-import { Cycles } from "../Cycles";
-import { DefaultButton } from "../DefaultButton";
-import { DefaultInput } from "../DefaultInput";
+import { PlayCircleIcon, StopCircleIcon } from 'lucide-react';
+import { Cycles } from '../Cycles';
+import { DefaultButton } from '../DefaultButton';
+import { DefaultInput } from '../DefaultInput';
+import { useRef } from 'react';
+import { useTaskContext } from '../../contexts/TaskContext/useTaskContext';
+import { getNextCycle } from '../../utils/getNextCycle';
+import { getNextCycleType } from '../../utils/getNextCycleType';
+import { TaskActionTypes } from '../../contexts/TaskContext/taskActions';
+import type { TaskModel } from '../../models/TaskModel';
 
 export function MainForm() {
-    const { state, setState } = useTaskContext();
+    const { state, dispatch } = useTaskContext();
     const taskNameInput = useRef<HTMLInputElement>(null);
 
+    // ciclos
     const nextCycle = getNextCycle(state.currentCycle);
-    const nextCycleType = getNextCycleType(nextCycle);
+    const nextCyleType = getNextCycleType(nextCycle);
 
     function handleCreateNewTask(event: React.FormEvent<HTMLFormElement>) {
         event.preventDefault();
@@ -34,60 +35,41 @@ export function MainForm() {
             startDate: Date.now(),
             completeDate: null,
             interruptDate: null,
-            duration: state.config[nextCycleType],
-            type: nextCycleType,
+            duration: state.config[nextCyleType],
+            type: nextCyleType,
         };
 
-        const secondsRemaining = newTask.duration * 60;
-
-        setState(prevState => {
-            return {
-                ...prevState,
-                config: { ...prevState.config },
-                activeTask: newTask,
-                currentCycle: nextCycle,
-                secondsRemaining, // Conferir
-                formattedSecondsRemaining: formatSecondsToMinutes(secondsRemaining), // Conferir
-                tasks: [...prevState.tasks, newTask],
-            };
-        });
+        dispatch({ type: TaskActionTypes.START_TASK, payload: newTask });
     }
 
     function handleInterruptTask() {
-        setState(prevState => {
-            return {
-                ...prevState,
-                activeTask: null,
-                secondsRemaining: 0,
-                formattedSecondsRemaining: '00:00',
-                tasks: prevState.tasks.map(task => {
-                    if (prevState.activeTask && prevState.activeTask.id === task.id) {
-                        return { ...task, interruptDate: Date.now() };
-                    }
-                    return task;
-                }),
-            };
-        });
+        dispatch({ type: TaskActionTypes.INTERRUPT_TASK });
     }
+
     return (
-        <form className="form" action="" onSubmit={handleCreateNewTask}>
-            <div className="formRow">
+        <form onSubmit={handleCreateNewTask} className='form' action=''>
+            <div className='formRow'>
                 <DefaultInput
-                    labelText="Task"
-                    id="myInput"
+                    labelText='task'
+                    id='meuInput'
                     type='text'
-                    placeholder="Enter your task"
+                    placeholder='Digite algo'
                     ref={taskNameInput}
                     disabled={!!state.activeTask}
                 />
             </div>
-            <div className="formRow">
+
+            <div className='formRow'>
                 <p>Próximo intervalo é de 25min</p>
             </div>
-            <div className="formRow">
-                <Cycles />
-            </div>
-            <div className="formRow">
+
+            {state.currentCycle > 0 && (
+                <div className='formRow'>
+                    <Cycles />
+                </div>
+            )}
+
+            <div className='formRow'>
                 {!state.activeTask && (
                     <DefaultButton
                         aria-label='Iniciar nova tarefa'
@@ -97,6 +79,7 @@ export function MainForm() {
                         key='botao_submit'
                     />
                 )}
+
                 {!!state.activeTask && (
                     <DefaultButton
                         aria-label='Interromper tarefa atual'
@@ -105,7 +88,7 @@ export function MainForm() {
                         color='red'
                         icon={<StopCircleIcon />}
                         onClick={handleInterruptTask}
-                        key='botao_interromper'
+                        key='botao_button'
                     />
                 )}
             </div>
--- Pomodoro/Pomodoro-Prototype/src/contexts/TaskContext/TaskContextProvider.tsx
+++ Pomodoro/Pomodoro-Prototype/src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,68 +1,22 @@
-import { useReducer, useState } from "react";
-import { inicialTaskState } from "./inicialTaskState";
-import { TaskContext } from "./TaskContext";
+import { useEffect, useReducer } from 'react';
+import { inicialTaskState } from './inicialTaskState';
+import { TaskContext } from './TaskContext';
+import { taskReducer } from './taskReducer';
 
 type TaskContextProviderProps = {
     children: React.ReactNode;
 };
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
+    const [state, dispatch] = useReducer(taskReducer, inicialTaskState);
 
-    const [state, setState] = useState(inicialTaskState);
-
-    type ActionType = {
-        type: string;
-        payload?: number;
-    };
-
-    const [myState, dispatch] = useReducer(
-        (state, action: ActionType) => {
-            console.log(state, action);
-
-            switch (action.type) {
-                case 'INCREMENT': {
-                    if (!action.payload) return state;
-
-                    return {
-                        ...state,
-                        secondsRemaing: state.secondsRemaing + action.payload,
-                    };
-                }
-                case 'DECREMENT': {
-                    if (!action.payload) return state;
-
-                    return {
-                        ...state,
-                        secondsRemaing: state.secondsRemaing - action.payload,
-                    };
-                }
-                case 'RESET': {
-                    return {
-                        secondsRemaing: 0,
-                    };
-                }
-            }
-
-            return state; // estado atual (não altera)
-        },
-        {
-            secondsRemaing: 0,
-        },
-    );
+    useEffect(() => {
+        console.log(state);
+    }, [state]);
 
     return (
-        <TaskContext.Provider value={{ state, setState }}>
-            <h1>O estado é: {JSON.stringify(myState)}</h1>
-            <button onClick={() => dispatch({ type: 'INCREMENT', payload: 10 })}>
-                Incrementar +10
-            </button>
-            <button onClick={() => dispatch({ type: 'INCREMENT', payload: 20 })}>
-                Incrementar +20
-            </button>
-            <button onClick={() => dispatch({ type: 'DECREMENT', payload: 50 })}>
-                Decrementar -50
-            </button>
-            <button onClick={() => dispatch({ type: 'RESET' })}>RESET</button>
+        <TaskContext.Provider value={{ state, dispatch }}>
+            {children}
         </TaskContext.Provider>
     );
 }
