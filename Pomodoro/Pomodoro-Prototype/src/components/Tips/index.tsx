2792dfe44c92247d9f93407bd02cbdb421db1fc3
Jeferson Tigik <jeftigik@hotmail.com>
2026-08-27 01:21:30 -0300

    Tips & TaskContextProvider updated

--- Pomodoro/Pomodoro-Prototype/src/components/Tips/index.tsx
+++ Pomodoro/Pomodoro-Prototype/src/components/Tips/index.tsx
@@ -9,19 +9,19 @@ export function Tips() {
 
     // Tips
     const tipsForWhenActiveTask = {
-        workTime: <span>Foque por {state.config.workTime}min</span>,
-        shortBreakTime: <span>Descanse por {state.config.shortBreakTime}min</span>,
+        workTime: <span>Foque por {state.config.workTime} min</span>,
+        shortBreakTime: <span>Descanse por {state.config.shortBreakTime} min</span>,
         longBreakTime: <span>Descanso longo</span>,
     };
 
     const tipsForNoActiveTask = {
         workTime: (
             <span>
-                Próximo ciclo é de <b>{state.config.workTime}min</b>
+                Próximo ciclo é de <b>{state.config.workTime} min</b>
             </span>
         ),
         shortBreakTime: (
-            <span>Próximo descaso é de {state.config.shortBreakTime}min</span>
+            <span>Próximo descaso é de {state.config.shortBreakTime} min</span>
         ),
         longBreakTime: <span>Próximo descanso será longo</span>,
     };
